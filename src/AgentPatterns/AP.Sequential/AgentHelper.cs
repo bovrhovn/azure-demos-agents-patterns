b98deb3e1ca1051379de2683880e6b57f82e7f96
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-04-01 13:53:56 +0200

    Add initial project structure with AgentHelper and translation functionality with SequentialWorkflow

---
+++ src/AgentPatterns/AP.Sequential/AgentHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+namespace AP.Sequential;
+
+public class AgentHelper(IChatClient client)
+{
+    internal ChatClientAgent GetTranslationAgent(string targetLanguage, IChatClient chatClient) =>
+        new(chatClient,
+            $"You are a translation assistant who only responds in {targetLanguage}. Respond to any " +
+            $"input by outputting the name of the input language and then translating the input to {targetLanguage}.");
+
+    public IEnumerable<ChatClientAgent> GetTranslationAgents() =>
+        from lang in (string[])["Slovenian", "Spanish", "English"]
+        select GetTranslationAgent(lang, client);
+}
